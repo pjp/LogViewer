b0dbb96f6714acf7a4e838e321c62cef61b02dbd
ppearce <paul@pearceful.net>
2016-05-19 13:32:55 +0100

    Added more command line overrides.

--- src/main/java/com/insight/utils/Utils.java
+++ src/main/java/com/insight/utils/Utils.java
@@ -363,20 +363,22 @@ public class Utils {
         if(args.length < 2) {
             System.err.println("View multiple log files in a single time ascending order list.");
             System.err.println("");
-            System.err.println("Usage: [@FILE] [@-] [#s#TS] [#e#TS] logfile logfile ...");
+            System.err.println("Usage: [@FILE] [@-] [=s=TS] [=e=TS] [#s#S] [#e#E] [#t#TS] logfile logfile ...");
             System.err.println("");
             System.err.println("   @-      Do not load any properties file.");
             System.err.println("   @FILE   A properties file to load configuration values from.");
-            System.err.println("   #s#TS   Set the starting TimeStamp (TS) for filtering log entries.");
-            System.err.println("   #e#TS   Set the ending TimeStamp (TS) for filtering log entries.");
-            System.err.println("");
+            System.err.println("   =s=TS   Set the starting TimeStamp (TS) for filtering log entries.");
+            System.err.println("   =e=TS   Set the ending TimeStamp (TS) for filtering log entries.");
+            System.err.println("   #s#S    Set the log entry TimeStamp leading character(s) to S.");
+            System.err.println("   #e#E    Set the log entry TimeStamp trailing character(s) to E.");
+            System.err.println("   #t#TS   Set the log entry TimeStamp formatter to TS");
             System.err.println("");
             System.err.println("Notes:");
             System.err.println("");
             System.err.println("The merged time ascending list is written to stdout.");
             System.err.println("");
-            System.err.println("Command line start/end TimeStamps override everything else, and HAVE to be in");
-            System.err.println("the same format as the value read from logentry.timestamp.date.format");
+            System.err.println("Command line values override everything else, and HAVE to be in");
+            System.err.println("the same format as the default values.");
             System.err.println("");
             System.err.println("By default, property file [" + propertyFileName + "] in the current") ;
             System.err.println("directory will be read if it exists. To prevent this, either explicitly");
@@ -398,6 +400,9 @@ public class Utils {
         boolean usingDefaultPropertyFile    = true;
         String cmdLineStartAt               = null;
         String cmdLineEndAt                 = null;
+        String cmdLineStartSentinal         = null;
+        String cmdLineEndSentinal           = null;
+        String cmdLineDateFormat            = null;
 
         for(String filename : args) {
             if (filename.startsWith("@-")) {
@@ -406,10 +411,16 @@ public class Utils {
             } else if (filename.startsWith("@")) {
                 propertyFileName = filename.substring(1);
                 usingDefaultPropertyFile = false;
-            } else if (filename.startsWith("#s#")) {
+            } else if (filename.startsWith("=s=")) {
                 cmdLineStartAt = filename.substring(3);
-            } else if (filename.startsWith("#e#")) {
-                cmdLineEndAt    = filename.substring(3);
+            } else if (filename.startsWith("=e=")) {
+                cmdLineEndAt = filename.substring(3);
+            } else if(filename.startsWith("#s#")) {
+                cmdLineStartSentinal = filename.substring(3);
+            } else if(filename.startsWith("#e#")) {
+                cmdLineEndSentinal = filename.substring(3);
+            } else if(filename.startsWith("=t=")) {
+                cmdLineDateFormat = filename.substring(3);
             } else {
                 logFiles.add(filename);
             }
@@ -439,6 +450,18 @@ public class Utils {
 
         if(null != cmdLineEndAt) {
             endAt = cmdLineEndAt;
+        }
+
+        if(null != cmdLineStartSentinal) {
+            timestampStartSentinal = cmdLineStartSentinal;
+        }
+
+        if(null != cmdLineEndSentinal) {
+            timestampEndSentinal = cmdLineEndSentinal;
+        }
+
+        if(null != cmdLineDateFormat) {
+            timestampDateFormat = cmdLineDateFormat;
         }
 
         List<List<LogEntry>> logs   = new ArrayList<>();
